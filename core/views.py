35201939dfac070c879c1a1984fc53acd3d77003
Febry Billiyagi <febrybilliyagi72@gmail.com>
2025-09-02 00:06:37 +0700

    feat: Adding events route

--- core/views.py
+++ core/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .models import User, Event, Registration, Ticket, TicketItem, Payment
+from .serializers import UserSerializer, EventSerializer, RegistrationSerializer, TicketSerializer, TicketItemSerializer, PaymentSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
+
+
 
 # Create your views here.
+class EventView(APIView):
+    def get(self, request):
+        events = Event.objects.all().order_by('created_at')[:10]
+        serializer = EventSerializer(events, many=True)
+        return Response({
+                'events': serializer.data
+            })
+    def post(self, request):
+        serializer = EventSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
